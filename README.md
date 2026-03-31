4ae4f19edd6a20ca9f94ce25c4dec55e63250ab4
L <yuzekesu@gmail.com>
2026-03-31 11:49:02 +0200

    Revert "delete everything"
    This reverts commit 82d98ab542e11f6742fddfbb2f8a6339ee5d08da.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Berkiba eats kebab
---
+++ server/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//DB-connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234", 
+  database: "social_test"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+//for registering endpoint, check for blanks, minimum password length, no duplicates
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  if (username.trim() === "" || password.trim() === "") {
+    return res.status(400).json({ error: "Fields cannot be empty" });
+  }
+
+  if (password.length <= 3) {
+    return res.status(400).json({ error: "Password must be at least 3 characters" });
+  }
+
+  db.query(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Database error (check yo username bruv)" });
+      }
+
+      if (result.length > 0) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+
+      //new user
+      db.query(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, password],
+        (err, result) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Database error (user prolly exists bruv)" });
+          }
+
+          //success
+          res.status(201).json({
+            message: "User created successfully",
+            userId: result.insertId
+          });
+        }
+      );
+    }
+  );
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
