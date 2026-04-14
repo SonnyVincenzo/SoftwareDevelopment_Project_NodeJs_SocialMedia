1c05b826fb509588a4bef4f1273cc8c60f2874f2
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-04-14 17:41:48 +0200

    add functioning like counter

--- public/js/like.js
+++ public/js/like.js
@@ -1,12 +1,13 @@
 const button = document.getElementsByClassName("like");
+const likes = document.getElementsByClassName("likes");
 for (let i = 0; i < button.length; i++) {
   let currButton = button[i];
+  let currLikes = likes[i];
   currButton.addEventListener("click", () => {
     console.log(button);
     currButton.style.color = "rgba(1, 199, 249, 1)";
-
+    value = parseInt(currLikes.getAttribute('value')) + 1;
+    currLikes.setAttribute('value', value)
+    currLikes.innerHTML = value;
   });
-
 }
-
-console.log("Js loded");
