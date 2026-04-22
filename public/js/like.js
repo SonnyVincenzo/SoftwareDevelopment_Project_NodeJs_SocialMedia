85f363589d5f59d16e7fc250ee903edc8bb24067
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-04-22 11:50:33 +0200

    attempted fix for likes and dislikes

--- public/js/like.js
+++ public/js/like.js
@@ -4,6 +4,7 @@ for (let i = 0; i < button.length; i++) {
   let currButton = button[i];
   let currLikes = likes[i];
   currButton.addEventListener("click", async () => {
+    
     const postId = currButton.dataset.postId;
     const res = await fetch("/like", {
       method: "POST",
@@ -14,22 +15,32 @@ for (let i = 0; i < button.length; i++) {
     });
     const data = await res.json();
     currLikes.innerHTML = data.likes;
+    currLikes.setAttribute('value', data.likes);
     
-    /*currButton.style.color = "rgba(1, 199, 249, 1)";
-    currButton.style.webkitTextStroke = "2px rgba(1, 199, 249, 1)";
-    value = parseInt(currLikes.getAttribute('value')) + 1;
-    currLikes.setAttribute('value', value)
-    currLikes.innerHTML = value;*/
-  });
+    if(currButton.userReaction === 'like') {
+      currButton.classList.add('active')
+      currButton.style.color = "rgba(1, 199, 249, 1)";
+      currButton.style.webkitTextStroke = "2px rgba(1, 199, 249, 1)";
+    } else {
+      currButton.classList.remove('active');
+      currButton.style.color = "" ;
+      currButton.style.webkitTextStroke = "2px blueviolet";
+    }
+  }); 
 }
 
 const dislikeButton = document.getElementsByClassName("dislike");
 const dislikes = document.getElementsByClassName("dislikes");
-for (let i = 0; i < button.length; i++) {
+for (let i = 0; i < dislikeButton.length; i++) {
   let currDislikeButton = dislikeButton[i];
   let currDislikes = dislikes[i];
   currDislikeButton.addEventListener("click", async () => {
-    const postId = currButton.dataset.postId;
+    currDislikeButton.style.color = "rgba(1, 199, 249, 1)";
+    currDislikeButton.style.webkitTextStroke = "2px rgba(1, 199, 249, 1)";
+    //value = parseInt(currDislikes.getAttribute('value')) + 1;
+    currDislikes.setAttribute('value', value)
+    currDislikes.innerHTML = value;
+    const postId = currDislikeButton.dataset.postId;
     const rest = await fetch("/dislike", {
       method: "POST",
       headers: {
@@ -38,12 +49,6 @@ for (let i = 0; i < button.length; i++) {
       body: JSON.stringify({postId})
     });
     const data = await rest.json();
-    currLikes.innerHTML = data.likes;
-    
-    /*currDislikeButton.style.color = "rgba(1, 199, 249, 1)";
-    currDislikeButton.style.webkitTextStroke = "2px rgba(1, 199, 249, 1)";
-    value = parseInt(currDislikes.getAttribute('value')) + 1;
-    currDislikes.setAttribute('value', value)
-    currDislikes.innerHTML = value;*/
+    currDislikes.innerHTML = data.likes;
   });
 }
