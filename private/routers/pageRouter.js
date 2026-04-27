94b787551f0d9ed168cc4ccdc47b5adf20c84105
LJkiller <OfficialLJkiller@gmail.com>
2026-04-27 16:09:34 +0200

    Fixes /home for displaying posts from db Resolves issue #169

--- private/routers/pageRouter.js
+++ private/routers/pageRouter.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 import { handleIndexGet } from '../routeHandlers/indexHandler.js';
 import { handleAboutGet } from '../routeHandlers/aboutHandler.js';
-import { handleHomeGet } from '../routeHandlers/homeHandler.js';
+import { createHomeGetHandler } from '../routeHandlers/homeHandler.js';
 import { createCommentPostHandler, createPostGetHandler, createPostPostHandler } from '../routeHandlers/postHandler.js';
 import { handlePostFeedGet, createPostFeedSnapshotHandler} from '../routeHandlers/postFeedHandler.js';
 
@@ -14,7 +14,7 @@ export default function createPageRouter(db) {
     // Set endpoints.
     router.get(['/', '/index'], handleIndexGet);
     router.get('/about', handleAboutGet);
-    router.get('/home', handleHomeGet);
+    router.get('/home', createHomeGetHandler(db));
     router.get('/post', createPostGetHandler(db));
     router.get('/post-feed', handlePostFeedGet);
     router.get('/post-feed/snapshot', createPostFeedSnapshotHandler(db));
