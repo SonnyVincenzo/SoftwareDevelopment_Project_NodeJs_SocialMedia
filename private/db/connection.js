1f698620bfdf9f8e028c313844773f8762fb4bbd
Berkant Hasan <hberkant28@gmail.com>
2026-04-29 01:36:34 +0200

    Added database initialization on server start (works even if tables are not created)

--- private/db/connection.js
+++ private/db/connection.js
@@ -6,6 +6,7 @@ const db = await mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    multipleStatements: true
 });
 
 console.log('Connected to MySQL.');
--- private/db/sql/table.likesDislikes.sql
+++ private/db/sql/table.likesDislikes.sql
@@ -1,5 +1,4 @@
-USE `social_test` ;
-CREATE TABLE `userLikesDislikes` (
+CREATE TABLE IF NOT EXISTS `userLikesDislikes` (
     `id` INT UNSIGNED NOT NULL,
     `username` VARCHAR(30) NOT NULL,
     type ENUM('likes','dislikes'),
@@ -8,10 +7,10 @@ CREATE TABLE `userLikesDislikes` (
     FOREIGN KEY (`username`) REFERENCES `User`(`username`)
 );
 
-CREATE TABLE `likesDislikes` (
+CREATE TABLE IF NOT EXISTS `likesDislikes` (
     `id` INT UNSIGNED NOT NULL,
     `likes` INT UNSIGNED,
-    `dislikes`INT UNSIGNED,
+    `dislikes` INT UNSIGNED,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`id`) REFERENCES `Posts`(`id`),
+    FOREIGN KEY (`id`) REFERENCES `Posts`(`id`)
 );
