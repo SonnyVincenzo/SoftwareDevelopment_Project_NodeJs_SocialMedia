f7dd207dd74ffd369ea7ad89ed2cd65048b49ee4
Berkant Resyul Hasan <habe24bs@student.ju.se>
2026-04-29 11:04:49 +0200

    Add db creation if db doesnt exist

--- private/db/connection.js
+++ private/db/connection.js
@@ -1,6 +1,21 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
+
+//Connect before db creation
+const initConnection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+
+});
+
+//Create db if it doesnt exist
+await initConnection.query(
+    `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`
+);
+
+//Connect to the actual db 
 const db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
