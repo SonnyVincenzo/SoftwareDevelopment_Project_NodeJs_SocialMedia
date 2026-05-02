4861950de034106da07c16d535bad1164cd47982
nasraabdirahman <nasrafaisal72@gmail.com>
2026-05-02 10:19:24 +0200

    added component test for DetectSwipeand added files that constains threshold and calculates difference for detectSwipe.js

--- public/js/detectSwipe.js
+++ public/js/detectSwipe.js
@@ -1,41 +1,49 @@
-import { showProfile } from "./profilePanel.js";
+import { difference } from './maxmin.js'
+import { trigger } from './trigger.js'
+import { showProfile } from './profilePanel.js';
 
 let startX = 0;
-
+//get main content area
 const target = document.querySelector(".post-board");
+//only run if element exists
 if(target)
 {
+
     document.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
+        //save starting position
+        startX = e.touches[0].clientX;
     });
     
     document.addEventListener("touchend", (e) => {
-    const endX = e.changedTouches[0].clientX;
-    const diff = endX - startX;
+        //get ending position
+        const endX = e.changedTouches[0].clientX;
+        //calculate swipe difference
+        const diff = difference(startX, endX);
+        //print distance 
+        console.log("swipe diff:", diff);
 
-    console.log("swipe diff:", diff);
+        //show profile panel if swiped larged enough
+        if (trigger(diff))
+            {
+                showProfile();
+            }
+    });
 
-    //swipe right threshold
-    if(diff > 80) {
-        showProfile();
-    }
-});
-
+    //this is for the mouse
     document.addEventListener("mousedown", (e) => {
         startX = e.clientX;
     });
     document.addEventListener("mouseup", (e) => {
+
         const endX = e.clientX;
-        const diff = endX - startX;
+        const diff = difference(startX, endX);
         
         console.log("mouse swipe diff:", diff);
         
-        if(diff > 80) {
+        if(trigger(diff)) {
             showProfile();
         }
     });
-
-
 }
 
 
