9f0c47917e8ebd455405ed4358080f52a9ea77ff
Zaid-Yassir Sultan <suza24gp@student.ju.se>
2026-05-06 11:34:52 +0200

    component test on signuphandler

--- component_test/app.test.js
+++ component_test/app.test.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import session from 'express-session';
 import { createLoginPostHandler } from '../private/routeHandlers/auth/loginHandler.js';
+import { createSignupPostHandler } from '../private/routeHandlers/auth/signupHandler.js';
 
 /** Creates test app for component testing.
  * 
@@ -19,6 +20,7 @@ export function createTestApp(mockDb) {
     }));
 
     app.post('/auth/login', createLoginPostHandler(mockDb)); // Login component test.
+    app.post('/auth/signup', createSignupPostHandler(mockDb)); // Signup component test.
 
     return app;
 }
