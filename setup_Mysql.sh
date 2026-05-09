109d221dbc2fa6847899309b7ab4c0ca41585e11
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2026-05-09 09:29:44 +0200

    fix sql script not working for the privilege granting

--- setup_Mysql.sh
+++ setup_Mysql.sh
@@ -3,7 +3,7 @@ cd ${APP_DIR}
 
 service mysql start
 mysql -uroot -e "CREATE USER ${DB_USER}@localhost IDENTIFIED BY ${DB_PASSWORD};"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_NAME}@localhost;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@localhost;"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
 # add the tables below here:
